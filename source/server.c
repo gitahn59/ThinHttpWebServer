e6bd0c756473505599d42c31aae630e167641fe0
gitahn59 <gooahn59@gmail.com>
2020-05-31 20:08:24 +0900

    Add server.c and Makefile for creating execuation file

---
+++ source/server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define CLISIZE 3
+#define MAXHTTPHEADSIZE 4096
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
